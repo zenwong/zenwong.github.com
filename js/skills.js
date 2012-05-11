4144349270a23199604111877c05dbee567e7df5
Zen Wong <zenwong@gmail.com>
2012-05-11 16:26:34 +0800

    testing

---
+++ js/skills.js
@@ -0,0 +1,21 @@
+var skills = {
+  database: {
+    redis: 90,
+    mysql: 60,
+    sqlserver: 50,
+    couchdb: 70,
+    mongodb: 50
+  },
+  frontend: {
+    html: 60,
+    javascript: 60,
+    jquery: 50,
+    photoshop: 40
+  },
+  backend: {
+    linux: 90,
+    windows: 70,
+    nginx: 80,
+    apache: 70
+  }
+};
